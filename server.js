a5c9bc88a28979eae21d94b45bec5aaa956470dc
Andres Wong <pipewong92@gmail.com>
2026-05-07 12:13:23 -0500

    Fix: agregar trust proxy para sesiones en Render

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set('trust proxy', 1);
+
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
